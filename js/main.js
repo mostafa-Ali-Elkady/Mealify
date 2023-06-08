e26d7d29cd247ac5dd6e91bc12a87f9f42df71eb
mostafa-Elkady <mostafaali789@gmail.com>
2023-06-08 21:08:48 +0300

    first upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Start Show Menu
+let links = document.querySelector(' ul.links'),
+  openMenu = document.querySelector('.icon-menu'),
+  closeMenu = document.querySelector('.close')
+openMenu.addEventListener('click', () => {
+  links.style.right = '0'
+})
+closeMenu.addEventListener('click', () => {
+  links.style.right = '-45%'
+})
+// End Show Menu
+// Start toogle active class on links
+let link = document.querySelectorAll("#links li a");
+link.forEach((list) => {
+  list.addEventListener("click", () => {
+    document.querySelector("#links li a.active").classList.remove("active");
+    list.classList.add("active")
+  });
+});
+// End toogle active class on links
+
+// Dark Mode
+let body = document.querySelector("body"),
+  dark = document.querySelector("#dark"),
+  waveImages = document.querySelectorAll(".wave");
+
+dark.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  waveImages.forEach((image) => {
+    if (body.classList.contains("dark-mode")) {
+      image.style.filter = "invert(0.91)"
+    } else {
+      image.style.filter = "invert(0)"
+    }
+  })
+
+
+
+})
